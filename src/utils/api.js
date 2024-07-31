aeece299f3b3ab50c7da8d5b7fafaa80928a2676
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-31 14:14:15 +0530

    refactor: Update fetchDataFromApi to use Google Custom Search API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+    key:'AIzaSyCc4Cy7MQWx4WODEIfgDPmjTYLjgiWRB3M',
+    cx:'b46d8966ba9274536'
+};
+
+export const fetchDataFromApi = async (payload) => {
+    const { data } = await axios.get(BASE_URL, {
+        params: { ...params, ...payload },
+    });
+    return data;
+};
